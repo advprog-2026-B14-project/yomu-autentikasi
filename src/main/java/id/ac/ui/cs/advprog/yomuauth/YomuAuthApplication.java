cdcfb7a44736a9f5c4015fc0aa3d7379a9893549
vd <mrafighalibf@gmail.com>
2026-02-20 00:29:23 +0700

    chore: initialize spring boot project

---
+++ src/main/java/id/ac/ui/cs/advprog/yomuauth/YomuAuthApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomuauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomuAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YomuAuthApplication.class, args);
+    }
+
+}
